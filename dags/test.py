336f0bc0b9de1b12e81d4851df335f06ae23f03e
김승훈 <eephus625@icloud.com>
2024-05-22 17:54:20 +0900

    second commit

--- dags/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://restcountries.com/v3/all'
-response = requests.get(url)
-data = response.json()
-for d in data:
-    print(d["name"]["official"])
-    print(d["population"])
-    print(d["area"])
